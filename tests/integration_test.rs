28e9f6252cc6896c665d12a86ec0d50c0eca570d
tomatenkuchen <t.w92@gmx.de>
2024-07-04 19:43:16 +0200

    renamed test to integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,8 +2,8 @@ use embedded_hal_mock::eh1::i2c::{Mock as I2cMock, Transaction as I2cTrans};
 use lps28dfw::{I2CAddress, Range, LPS28DFW};
 use uom::si::f32::Pressure;
 
-#[cfg(test)]
-fn embedded_hal_mock_test() -> () {
+#[test]
+fn test_embedded_hal_mock_test() {
     let expectations = [
         I2cTrans::write(0x5C, vec![0x28]),
         I2cTrans::read(0x5C, vec![0xFF, 0xFF, 0xFF]),
