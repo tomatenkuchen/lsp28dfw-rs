eab1103649fc9daeec9332ba88170955df9fdd62
tomatenkuchen <t.w92@gmx.de>
2024-07-05 22:36:57 +0200

    removed unused mot

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,7 +4,7 @@ extern crate uom;
 
 use embedded_hal_mock::eh1::i2c::{Mock as I2cMock, Transaction as I2cTrans};
 use lps28dfw::{I2CAddress, Range, LPS28DFW};
-use uom::si::{f32::*, pressure::hectopascal, temperature_interval::degree_celsius};
+use uom::si::pressure::hectopascal;
 
 #[test]
 fn test_embedded_hal_mock_test() {
@@ -12,7 +12,7 @@ fn test_embedded_hal_mock_test() {
         I2cTrans::write(0x5C, vec![0x28]),
         I2cTrans::read(0x5C, vec![0xFF, 0xFF, 0xFF]),
     ];
-    let mut i2c = I2cMock::new(&expectations);
+    let i2c = I2cMock::new(&expectations);
 
     let mut sens_p = LPS28DFW::new(i2c, I2CAddress::Low, Range::Range1260hPa);
     let p_meas = sens_p.read_pressure();
